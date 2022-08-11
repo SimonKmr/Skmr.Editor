4276a7fabb3aedc3fa3e8943c1eadc5bd5a0e307
SimonKmr <darkfish070@gmail.com>
2022-08-11 08:00:27 +0200

    unified culture info

--- Skmr.Editor/Instructions/ColorGradeVideo.cs
+++ Skmr.Editor/Instructions/ColorGradeVideo.cs
@@ -26,17 +26,18 @@ namespace Skmr.Editor.Instructions
 
         public void Execute()
         {
+            var ci = new CultureInfo("en-US");
             StringBuilder sb = new StringBuilder();
             sb.Append($"-i {Input.Path} ");
             sb.Append("-vf eq=");
-            sb.Append($"contrast={Contrast.ToString(new CultureInfo("en-US"))}:");
-            sb.Append($"brightness={Brighness.ToString(new CultureInfo("en-US"))}:");
-            sb.Append($"saturation={Saturation.ToString(new CultureInfo("en-US"))}:");
-            sb.Append($"gamma={Gamma.ToString(new CultureInfo("en-US"))}:");
-            sb.Append($"gamma_r={GammaR.ToString(new CultureInfo("en-US"))}:");
-            sb.Append($"gamma_g={GammaG.ToString(new CultureInfo("en-US"))}:");
-            sb.Append($"gamma_b={GammaB.ToString(new CultureInfo("en-US"))}:");
-            sb.Append($"gamma_weight={GammaWeight.ToString(new CultureInfo("en-US"))}");
+            sb.Append($"contrast={Contrast.ToString(ci)}:");
+            sb.Append($"brightness={Brighness.ToString(ci)}:");
+            sb.Append($"saturation={Saturation.ToString(ci)}:");
+            sb.Append($"gamma={Gamma.ToString(ci)}:");
+            sb.Append($"gamma_r={GammaR.ToString(ci)}:");
+            sb.Append($"gamma_g={GammaG.ToString(ci)}:");
+            sb.Append($"gamma_b={GammaB.ToString(ci)}:");
+            sb.Append($"gamma_weight={GammaWeight.ToString(ci)}");
             sb.Append($" {Output.Path}");
 
             Ffmpeg.Run(sb.ToString());
