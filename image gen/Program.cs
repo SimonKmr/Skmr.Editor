2b07acba73ae41932db16a6cdc8cd1a827fa1eec
SimonKmr <darkfish070@gmail.com>
2023-07-01 19:48:35 +0200

    add y4m

---
+++ image gen/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Skmr.Editor.Images;
+using Skmr.Editor.Images.Patterns;
+
+ICommand[] commands =
+{
+    new Grid()
+    {
+        TileSize = 50,
+        Color = (0,0, 0),
+    }
+};
+Renderer.Save(1920 * 4, 1080 * 4, "grid_black.png", commands);
