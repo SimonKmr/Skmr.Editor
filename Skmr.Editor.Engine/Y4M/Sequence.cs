30a4bcb4f4c1d6cd100948730a35cf3de82d88ba
SimonKmr <darkfish070@gmail.com>
2023-07-23 00:27:54 +0200

    add interface implementation add Provide Frame Method

--- Skmr.Editor.Engine/Y4M/Sequence.cs
+++ Skmr.Editor.Engine/Y4M/Sequence.cs
@@ -17,5 +17,23 @@
         public int FrameRate { get; set; }
         public string ColorSpace { get; set; }
         private Frame[] Frames { get; set; }
+
+        public int Size
+            => (FrameBodySize + FrameHeaderSize) * Frames.Length;
+        public int FrameSize
+            => FrameBodySize + FrameHeaderSize;
+        public int FrameBodySize
+            => Width * Height * 3 / 2;
+        public int FrameHeaderSize
+            => "FRAME\n".Length;
+
+        /// <summary>
+        /// Creates a frame for the sequence
+        /// </summary>
+        /// <returns></returns>
+        public Frame ProvideFrame()
+        {
+            return new Frame(this);
+        }
     } 
 }
