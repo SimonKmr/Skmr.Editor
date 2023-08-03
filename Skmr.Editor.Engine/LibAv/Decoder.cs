39332afed3c6ae9f6fc137b2e569c8a444627227
SimonKmr <41740705+SimonKmr@users.noreply.github.com>
2023-08-03 22:34:34 +0200

    add Ffmpeg dlls

---
+++ Skmr.Editor.Engine/LibAv/Decoder.cs
@@ -0,0 +1,14 @@
+﻿using FFmpeg.AutoGen;
+
+namespace Skmr.Editor.Engine.LibAv
+{
+    public class Decoder
+    {
+        public Decoder() 
+        {
+            ffmpeg.RootPath = "Dlls";
+            var v = ffmpeg.avcodec_version();
+            Console.WriteLine(v);
+        }
+    }
+}
