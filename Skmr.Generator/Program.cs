9f56d9c2c1f0ce38df5660a1ed9c9f563f2adfd1
SimonKmr <darkfish070@gmail.com>
2023-09-10 15:46:29 +0200

    Add Codegenerator for Rav1e

---
+++ Skmr.Generator/Program.cs
@@ -0,0 +1,11 @@
+﻿using CppSharp;
+using Skmr.Generator;
+
+Console.WriteLine("Generating Rav1e Bindings...");
+Console.WriteLine();
+
+ConsoleDriver.Run(new Rav1eLibrary());
+
+Console.WriteLine();
+Console.WriteLine("Generating openh264 Bindings...");
+Console.WriteLine();
