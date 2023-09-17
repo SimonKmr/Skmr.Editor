e025834df0622bce80ac9a981cd192d515779484
SimonKmr <darkfish070@gmail.com>
2023-09-17 12:06:19 +0200

    remove libav this is cause we use FFmpeg.AutoGen

--- Skmr.Generator/Program.cs
+++ Skmr.Generator/Program.cs
@@ -5,7 +5,3 @@ Console.WriteLine("Generating Rav1e Bindings...");
 Console.WriteLine();
 
 ConsoleDriver.Run(new Rav1eLibrary());
-
-Console.WriteLine();
-Console.WriteLine("Generating openh264 Bindings...");
-Console.WriteLine();
