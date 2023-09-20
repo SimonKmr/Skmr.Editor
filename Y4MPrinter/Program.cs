7f655cc3b6393412129a7a95298680d57de65376
SimonKmr <41740705+SimonKmr@users.noreply.github.com>
2023-09-20 21:53:28 +0200

    testing

--- Y4MPrinter/Program.cs
+++ Y4MPrinter/Program.cs
@@ -1,40 +1,73 @@
-﻿using LibAv = Skmr.Editor.Engine.LibAv;
-using Skmr.Editor.Engine.Rav1e.Api;
-using Rav1e = Skmr.Editor.Engine.Rav1e;
+﻿using Rav1e = Skmr.Editor.Engine.Rav1e;
 using Y4M = Skmr.Editor.Engine.Y4M;
+using Skmr.Editor.Engine.OpenH264;
 
-string path = @"C:\Users\darkf\Desktop\output - Kopie.y4m";
-string output = @"test3.ivf";
+int width = 480;
+int height = 270;
+Y4M.Frame frame;
+TestOpenH264();
+TestRav1e();
+Console.WriteLine("Done!");
+while (true) ;
 
-LibAv.Decoder decoder = new LibAv.Decoder();
-
-return;
-
-var test = new Y4M.File(path);
-
-using (Stream source = File.Open(output, FileMode.Create))
+void TestOpenH264()
 {
-    using (var rav1e = new Rav1e.Encoder(1920, 1080, 30))
-    {
-        int i = 0;
+    string h264Test = @"C:\Users\Simon\Desktop\res.h264";
+    int i = 0;
+    Decoder decoder = new Decoder(width, height);
 
-        foreach (var f in rav1e.ReceiveFrame())
+    using (Stream h264 = File.Open(h264Test, FileMode.Open))
+    {
+        Reader reader = new Reader(h264);
+        while (true)
         {
-            if (i > 300)
+            var data = reader.ReadFrame();
+            if (data.Length == 0) continue;
+
+            if (decoder.TryDecode(data, out frame))
             {
-                rav1e.Flush();
-            }
-            else if (f.status == EncoderStatus.NeedMoreData)
-            {
-                rav1e.SendFrame(test[i]);
+                if(i > 5)
+                {
+                    break;
+                }
                 i++;
-            }
-            else if (f.status == EncoderStatus.Success && f.data != null)
-            {
-                source.Write(f.data, 0, f.data.Length);
             }
         }
     }
 }
 
-while (true) ;
+void TestRav1e()
+{
+    //Setup
+    string output = @"test3.ivf";
+
+    //Encoding
+    using (Stream source = File.Open(output, FileMode.Create))
+    {
+        using (var rav1e = new Rav1e.Encoder(width, height, 30))
+        {
+            int i = 0;
+
+            while (true)
+            {
+                var status = rav1e.ReceiveFrame(out byte[]? data);
+
+                if (status == Rav1e.Api.EncoderStatus.LimitReached) break;
+
+                if (i > 300)
+                {
+                    rav1e.Flush();
+                }
+                else if (status == Rav1e.Api.EncoderStatus.NeedMoreData)
+                {
+                    rav1e.SendFrame(frame);
+                    i++;
+                }
+                else if (status == Rav1e.Api.EncoderStatus.Success && data != null)
+                {
+                    source.Write(data, 0, data.Length);
+                }
+            }
+        }
+    }
+}
