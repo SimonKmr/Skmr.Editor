03a64fe4c8629e0b77b1906a2587684b1125b3d9
SimonKmr <41740705+SimonKmr@users.noreply.github.com>
2023-09-20 21:53:13 +0200

    add StreamReader and StreamWriter implementation for Y4M files

---
+++ Skmr.Editor.Engine/Y4M/StreamReader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skmr.Editor.Engine.Y4M
+{
+    public class StreamReader : IDisposable
+    {
+        private int width = 480;
+        private int height = 270;
+        private FileStream _fileStream;
+        
+        public StreamReader(string path) 
+        {
+            _fileStream = File.Open(path, FileMode.Open);
+
+            while (true)
+            {
+                var value = _fileStream.ReadByte();
+                var breakChar = (int)'\n';
+                if (value == breakChar) break;
+            }
+            
+            var end = (int)_fileStream.Position+1;
+            _fileStream.Position = 0;
+
+            var buffer = new byte[end];
+            _fileStream.Read(buffer, 0, end);
+        }
+
+        public void Read(out Frame frame)
+        {
+            while (_fileStream.ReadByte() != '\n');
+            var buffer = new byte[width * height * 3 / 2];
+            _fileStream.Read(buffer, 0, buffer.Length);
+            frame = new Frame(width, height, buffer);
+        }
+
+        public void Dispose()
+        {
+            _fileStream.Dispose();
+        }
+    }
+}
