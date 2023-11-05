948f9474a384ae14d41e3d13501d85b4d567491a
SimonKmr <41740705+SimonKmr@users.noreply.github.com>
2023-11-05 11:42:04 +0100

    removed old using directives

--- Y4MPrinter/Program.cs
+++ Y4MPrinter/Program.cs
@@ -1,14 +1,4 @@
-﻿using Y4M = Skmr.Editor.Engine.Y4M;
-using Skmr.Editor.Engine.Codecs.Apis.Rav1e;
-using Skmr.Editor.Engine.Bitstreams.H264;
-using Skmr.Editor.Engine.Codecs;
-using H264 = Skmr.Editor.Engine.Bitstreams.H264;
-using OpenH264Sample;
-using System.Drawing;
-using Skmr.Editor.Engine.Y4M;
-using Skmr.Editor.Engine;
-using System;
-using Skmr.Editor.Engine.Containers.Mp4;
+﻿using Skmr.Editor.Engine.Containers.Mp4;
 
 var file = new Mp4File("C:\\Users\\Simon\\Desktop\\test.mp4");
 var atoms = file.Read();
