5f2d74b29101bb37b651992772945baaa718a350
SimonKmr <darkfish070@gmail.com>
2024-07-07 11:53:50 +0200

    format

--- Skmr.Editor.MotionGraphics/Function.cs
+++ Skmr.Editor.MotionGraphics/Function.cs
@@ -14,7 +14,6 @@ namespace Skmr.Editor.MotionGraphics
             var t = Math.Pow(float.E, -k * (x - 0.5));
             return 1 / (1 + (float)t);
         }
-
         public static float Linear(float x) => x;
         public static float Square(float x) => 1 - (x - 1) * (x - 1);
         public static float Cubic(float x) => (x - 1) * (x - 1) * (x - 1) + 1;
