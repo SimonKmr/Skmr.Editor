0212b46af7830554fca280327df5661d9a0941c9
SimonKmr <darkfish070@gmail.com>
2024-07-07 11:53:05 +0200

    add sequence to Engine

---
+++ Skmr.Editor.Data/Interfaces/ISequence.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skmr.Editor.Data.Interfaces
+{
+    public interface ISequence
+    {
+        public int StartFrame { get; set; }
+        public int EndFrame { get; set; }
+
+        public void Render();
+    }
+}
---
+++ Skmr.Editor.Engine/Sequence.cs
@@ -0,0 +1,25 @@
+﻿using Skmr.Editor.Data.Interfaces;
+using Skmr.Editor.Engine.Codecs;
+using Skmr.Editor.Engine.Containers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skmr.Editor.Engine
+{
+    public class Sequence : ISequence
+    {
+        public int StartFrame { get; set; }
+        public int EndFrame { get; set; }
+
+        IVideoContainer video;
+        IVideoDecoder decoder;
+
+        public void Render()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
