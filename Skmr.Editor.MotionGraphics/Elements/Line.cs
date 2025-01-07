9b5c86db1aa7164cde77e1b0bc1caf08541ce9d7
SimonKmr <darkfish070@gmail.com>
2025-01-08 00:15:32 +0100

    add line class

---
+++ Skmr.Editor.MotionGraphics/Elements/Line.cs
@@ -0,0 +1,20 @@
+﻿using SkiaSharp;
+using Skmr.Editor.Data;
+using Skmr.Editor.MotionGraphics.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skmr.Editor.MotionGraphics.Elements
+{
+    public class Line : IElement
+    {
+        public IAttribute<Vec2D>[] Points { get; set; }
+        public void DrawOn(int frame, SKCanvas canvas)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
