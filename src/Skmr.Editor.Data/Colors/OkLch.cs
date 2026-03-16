0b8ecd0385a636f22231cbc91a447b7f5fa47195
SimonKmr <41740705+SimonKmr@users.noreply.github.com>
2026-03-16 22:29:19 +0100

    renamed from OkLab to OkLch

---
+++ src/Skmr.Editor.Data/Colors/OkLch.cs
@@ -0,0 +1,21 @@
+﻿using Skmr.Editor.Data.Interfaces;
+
+namespace Skmr.Editor.Data.Colors;
+
+public struct OkLch : IDefault<OkLch>
+{
+    public OkLch(double l, double c, double h)
+    {
+        Lightness = l;
+        Chroma = c;
+        Hue = h;
+    }
+    public double Lightness;
+    public double Chroma;
+    public double Hue;
+    
+    public static OkLch GetDefault()
+    {
+        return new OkLch(0, 0, 0);
+    }
+}
